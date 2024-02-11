1ead12f195c2625355cf1fd027d2e8c3ecd9083a
allelbhagya <allel.bhagya@gmail.com>
2024-02-11 18:32:49 +0530

    search, login page

--- videoapp/views.py
+++ videoapp/views.py
@@ -2,6 +2,7 @@ import os
 import cv2
 from django.shortcuts import render, redirect
 from django.conf import settings
+import datetime
 
 def upload_video(request):
     if request.method == 'POST' and request.FILES['video_file']:
@@ -11,19 +12,17 @@ def upload_video(request):
             for chunk in video_file.chunks():
                 destination.write(chunk)
         
-        # Convert the uploaded video to MPEG-4 AAC and H.264
+        # Convert the uploaded video to black and white
         input_video_path = os.path.join(settings.MEDIA_ROOT, 'videos', video_file.name)
         output_video_path = os.path.join(settings.MEDIA_ROOT, 'videos', 'bw_' + video_file.name)
         if convert_video(input_video_path, output_video_path):
-            return redirect('play_video', video_name='bw_' + video_file.name)
+            # Render the play.html template with the newly converted video URL
+            video_url = os.path.join(settings.MEDIA_URL, 'videos', 'bw_' + video_file.name)
+            return render(request, 'play.html', {'video_url': video_url})
         else:
             return render(request, 'upload.html', {'error': 'Failed to convert video.'})
     
     return render(request, 'upload.html')
-
-def play_video(request, video_name):
-    video_url = os.path.join(settings.MEDIA_URL, 'videos', video_name)
-    return render(request, 'play.html', {'video_url': video_url})
 
 def convert_video(video_path, output_path):
     cap = cv2.VideoCapture(video_path)
@@ -35,12 +34,10 @@ def convert_video(video_path, output_path):
     frame_width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
     frame_height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
     
-    # Codec for MPEG-4 AAC
-    fourcc_aac = cv2.VideoWriter_fourcc(*'mp4a')
     # Codec for H.264
     fourcc_h264 = cv2.VideoWriter_fourcc(*'avc1')
     
-    # Use one of the above fourcc for output video
+    # Use H.264 codec for output video
     out = cv2.VideoWriter(output_path, fourcc_h264, fps, (frame_width, frame_height), isColor=False) # Set isColor=False for grayscale
     
     while True:
@@ -62,8 +59,10 @@ def play_video(request):
     video_dir = os.path.join(settings.MEDIA_ROOT, 'videos')
     for filename in os.listdir(video_dir):
         if filename.startswith('bw_'):
-            bw_videos.append(filename)
-    
+            file_path = os.path.join(video_dir, filename)
+            creation_date = os.path.getctime(file_path)  # Get the creation timestamp
+            bw_videos.append((filename, datetime.datetime.fromtimestamp(creation_date)))
+
     return render(request, 'play_list.html', {'bw_videos': bw_videos})
 
 def play_selected_video(request, video_name):
